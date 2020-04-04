dcc23d09996842e286d7c6884e86b0fc1a734af3
Erik Sillén <erik.sillen@omegapoint.se>
2020-04-04 17:06:40 +0200

    Changed index text

--- routes/index.js
+++ routes/index.js
@@ -3,7 +3,7 @@ var router = express.Router();
 
 /* GET home page. */
 router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Descripto' });
+  res.render('index', { title: 'Bussallokering' });
 });
 
 router.get('/word', function(req, res, next) {
